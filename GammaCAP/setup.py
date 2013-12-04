46720fdc351b99e360935ee64cd3bd27548445c4
carlson <erccarls@ucsc.edu>
2013-12-04 04:18:56 -0800

    Added matplotlib dep. to setup.py

--- GammaCAP/setup.py
+++ GammaCAP/setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='gammacap',
-      version='0.9.06',
+      version='0.9.07',
       description='Gamma Ray Cluster Analysis Package',
       long_description='The gammacap package provides statistical tools and clustering algorithms for analysis of gamma-ray data and is an acronym for "Gamma-ray Clustering Analysis Package. It is actively maintained and written in pure python, requiring only standard scientific libraries.',
       author='Eric Carlson',
       author_email='erccarls@ucsc.edu',
       url='http://planck.ucsc.edu/gammacap',
-      keywords=['Requires: numpy','Requires: scikit-learn','Requires: pyfits'], 
+      keywords=['Requires: numpy','Requires: scikit-learn','Requires: pyfits','Requires:matplotlib'], 
       #package_dir={'': './'},
       packages = ["GammaCAP","GammaCAP.BGTools","GammaCAP.Stats"]
      )
